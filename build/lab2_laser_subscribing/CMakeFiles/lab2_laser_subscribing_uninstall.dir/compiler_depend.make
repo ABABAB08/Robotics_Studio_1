fdfd38565fab6e3bd8dc79cc9702db70001a451b
Ubuntu <student@labMGJSX0.dibj4albw4huhoiqswueuf4m3d.px.internal.cloudapp.net>
2024-08-15 08:50:18 +0000

    week2

---
+++ build/lab2_laser_subscribing/CMakeFiles/lab2_laser_subscribing_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for lab2_laser_subscribing_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/lab2_laser_subscribing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/Desktop/Robotics_Studio_1/Robotics_Studio_1/src/lab2_laser_subscribing
+# Build directory: /home/student/Desktop/Robotics_Studio_1/Robotics_Studio_1/build/lab2_laser_subscribing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
